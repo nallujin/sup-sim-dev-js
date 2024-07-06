562dad08621c7b56671341e0f21d51c6489dc5b7
nallujin <benedictuspujagarfield@gmail.com>
2024-07-07 00:21:37 +0700

    Change the for loop to forEach

---
+++ 12-advanced-functions/12-todo-list/12-todo-list.js
@@ -0,0 +1,46 @@
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+const btn1 = document.querySelector("#add1");
+btn1.addEventListener("click", addTodo);
+renderTodoList();
+
+function addTodo() {
+    const inputElement = document.querySelector("#nameInput");
+    const name = document.querySelector("#nameInput").value;
+
+    const dateInputElement = document.querySelector("#dueDateInput");
+    const dueDate = document.querySelector("#dueDateInput").value;
+
+    todoList.push({name, dueDate});
+
+    inputElement.value = "";
+    dateInputElement.value = "";
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    renderTodoList()
+}
+
+function renderTodoList() {
+    const divTodo = document.querySelector("#todo-1");
+    divTodo.innerHTML = "";
+    todoList.forEach((task, i)=>{
+        const taskName = document.createElement("div");
+        const taskDue = document.createElement("div");
+
+        const {name, dueDate} = task;
+        taskName.textContent = name;
+        taskDue.textContent = dueDate;
+
+        const delBtn = document.createElement("button");
+        delBtn.textContent = "Delete";
+        delBtn.classList.add("delete-todo-button")
+        delBtn.addEventListener("click", () => {
+            todoList.splice(i, 1);
+            renderTodoList();
+            localStorage.setItem("todoList", JSON.stringify(todoList));
+        });
+
+        divTodo.appendChild(taskName);
+        divTodo.appendChild(taskDue);
+        divTodo.appendChild(delBtn);
+    })
+}
